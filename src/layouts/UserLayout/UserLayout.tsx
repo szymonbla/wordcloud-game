13d6ed227b108997056d4075f22c3cfa7f7cab30
Szymon Błażyński <sz.blazynski@gmail.com>
2022-05-05 17:20:05 +0200

    AB#43 Implement top bar without user section (#2)

--- src/layouts/UserLayout/UserLayout.tsx
+++ src/layouts/UserLayout/UserLayout.tsx
@@ -2,13 +2,21 @@ import { ReactNode } from 'react';
 
 import { Grid } from '@mui/material';
 
+import { TopBar } from 'layouts/components';
 interface UserLayoutProps {
   children: ReactNode;
 }
 
 export const UserLayout = ({ children }: UserLayoutProps) => {
   return (
-    <Grid container sx={{ width: '100vw', height: '100vh', position: 'relative' }}>
+    <Grid
+      container
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ width: '100vw', height: '100vh', position: 'relative' }}
+    >
+      <TopBar />
       {children}
     </Grid>
   );
