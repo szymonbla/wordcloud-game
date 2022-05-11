6d244daeee6a3078e0b6dbe1a61c800a1e00c208
Szymon Błażyński <sz.blazynski@gmail.com>
2022-05-11 13:10:53 +0200

    AB#34 Piece login page together (#5)

--- src/features/login/components/Login.tsx
+++ src/features/login/components/Login.tsx
@@ -1,11 +1,17 @@
-import { Grid } from '@mui/material';
-import { InputField, ButtonComponent } from 'common/components';
+import { Grid, Typography } from '@mui/material';
+
+import { ButtonComponent, FormTextField } from 'common/components';
 
 export const Login = () => {
   return (
-    <Grid>
-      <InputField label="Nickname" name="nickname" type="text" />
-      <ButtonComponent label="Play" />
+    <Grid display="flex" flexDirection="column" sx={{ '& > div': { mb: 4 } }}>
+      <Typography variant="h2" fontWeight="600" sx={{ textAlign: 'center', mb: 4 }}>
+        WordCloud Game
+      </Typography>
+      <FormTextField label="Nickname" name="nickname" type="text" placeholder="Enter your nickname here..." />
+      <Grid display="flex" justifyContent="center" alignItems="center">
+        <ButtonComponent label="Play" sx={{ width: '30%' }} />
+      </Grid>
     </Grid>
   );
 };
